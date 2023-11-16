ae1ddd2617c9fbd4f920f00fa073c1f42e34d84f
esojogerba <esojogerba@users.noreply.github.com>
2023-11-16 01:06:18 -0600

    JS cta button redirects to sing up page.

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+// Create account button.
+const newAccountBtn = document.getElementById("new-account-btn");
+
+// Redirect to sign up when newAccountBtn is clicked.
+newAccountBtn.addEventListener("click", () => {
+    location.href = "/html/sign-up.html";
+});
