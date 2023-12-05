002bd43f839bd1d11577ad1564aad4e9b9316995
esojogerba <esojogerba@users.noreply.github.com>
2023-12-04 18:08:05 -0600

    JS updated variable names.

--- assets/js/api.js
+++ assets/js/api.js
@@ -4,13 +4,13 @@
 import { key } from "./key.js";
 
 // Constant variables.
-const api_key = key;
+const API_KEY = key;
 const imageBaseURL = "http://image.tmdb.org/t/p/";
 
 // Fetches data from API using a url.
 // Passes the results in JSON format into a callback function,
 // along with an optional parameter if applicable.
-const fetchDataFromServer = function (url, callback, optionalParam) {
+const fetchDataFromAPI = function (url, callback, optionalParam) {
     // Fetch data from URL.
     fetch(url)
         // Format results into JSON format.
@@ -20,4 +20,4 @@ const fetchDataFromServer = function (url, callback, optionalParam) {
 };
 
 // Export functionalities and constant variables.
-export { imageBaseURL, api_key, fetchDataFromServer };
+export { imageBaseURL, API_KEY, fetchDataFromAPI };
