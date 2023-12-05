7d2d661f4d2005f03cbd8fd0d643693c98c1bc42
esojogerba <esojogerba@users.noreply.github.com>
2023-12-04 22:36:16 -0600

    movies.js initiated.

---
+++ assets/js/movies.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// Imports
+import { API_KEY, imageBaseURL, fetchDataFromAPI } from "./api.js";
+
+// Retrieves page-content <article> from movies page.
+const pageContent = document.querySelector("[page-content]");
