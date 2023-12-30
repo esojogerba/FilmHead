71e18693d15496ef2f6e55cece4dae3e1ee3c1db
esojogerba <esojogerba@users.noreply.github.com>
2023-12-29 21:12:21 -0600

    createMediaCard function initiated.

--- assets/js/media-card.js
+++ assets/js/media-card.js
@@ -1,3 +1,7 @@
 "user strict";
 
 // Imports
+import { imageBaseURL } from "./api";
+
+// Creates a media card using the data collected from the API.
+export function createMediaCard(type, mediaData) {}
