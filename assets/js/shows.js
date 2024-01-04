7eac72a2c27851f7be856fbae4fd9bcf0ebeaf4b
esojogerba <esojogerba@users.noreply.github.com>
2024-01-04 02:08:56 -0600

    Retrieve genres.

--- assets/js/shows.js
+++ assets/js/shows.js
@@ -6,3 +6,37 @@ import { createMediaCard } from "./media-card.js";
 
 // Retrieves page-content <article> from movies page.
 const pageContent = document.querySelector("[page-content]");
+
+// Fetch all genres. Example: [ { "id": "123", "name": "Action" } ]
+// Then change genre format to {123: "Action"}
+const genreList = {
+    // Assign correct genre string to each genre_id provided. Example: [23 , 43] = "Action, Romance".
+    asString(genreIdList) {
+        // Will hold list of genre strings.
+        let newGenreList = [];
+
+        for (const genreId of genreIdList) {
+            // If current genreId exists in genreList, push it to newGenreList.
+            // this == genreList
+            this[genreId] && newGenreList.push(this[genreId]);
+        }
+        return newGenreList.join(" · ");
+    },
+};
+
+// Retrieves all genres from API.
+fetchDataFromAPI(
+    `https://api.themoviedb.org/3/genre/tv/list?api_key=${API_KEY}`,
+
+    function ({ genres }) {
+        for (const { id, name } of genres) {
+            genreList[id] = name;
+        }
+
+        // Retrieves popular movie data and passes it in JSON format to heroBanner().
+        fetchDataFromAPI(
+            `https://api.themoviedb.org/3/tv/popular?api_key=${API_KEY}&page=1`,
+            heroBanner
+        );
+    }
+);
