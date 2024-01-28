96b0fdfa36efea654ad63aed494cfc7aa6dc2b4e
esojogerba <esojogerba@users.noreply.github.com>
2024-01-27 22:10:04 -0600

    Retrieve movieId from local storage and print it.

--- assets/js/movie-details.js
+++ assets/js/movie-details.js
@@ -3,3 +3,8 @@
 // Imports
 import { API_KEY, imageBaseURL, fetchDataFromAPI } from "./api.js";
 import { createMediaCard } from "./media-card.js";
+
+// Retrieves the saved movieId of the movie selected from local storage.
+const movieId = window.localStorage.getItem("movieId");
+
+console.log(movieId);
