00e54a8f4d82f971fe5a2a9be4ae42b6dc9fa2a6
esojogerba <esojogerba@users.noreply.github.com>
2024-01-30 22:19:31 -0600

    Store movie details.

--- assets/js/movie-details.js
+++ assets/js/movie-details.js
@@ -15,5 +15,22 @@ const pageContent = document.querySelector("[page-content]");
 // Retrieves movie details using the provided movieId.
 fetchDataFromAPI(
     `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}&append_to_response=casts,videos,images,releases`,
-    function (movie) {}
+    function (movie) {
+        // Stores data for current movie into movie.
+        const {
+            backdrop_path,
+            poster_path,
+            title,
+            release_date,
+            runtime,
+            vote_average,
+            releases: {
+                countries: [{ certification }],
+            },
+            genres,
+            overview,
+            casts: { cast, crew },
+            videos: { results: videos },
+        } = movie;
+    }
 );
