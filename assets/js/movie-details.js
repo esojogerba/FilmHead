bea25f33094f5d015d6c56d32eee218cc2b431a9
esojogerba <esojogerba@users.noreply.github.com>
2024-01-30 22:18:55 -0600

    Retrieve movie details initiated.

--- assets/js/movie-details.js
+++ assets/js/movie-details.js
@@ -11,3 +11,9 @@ console.log(movieId);
 
 // Retrieves the page's content.
 const pageContent = document.querySelector("[page-content]");
+
+// Retrieves movie details using the provided movieId.
+fetchDataFromAPI(
+    `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}&append_to_response=casts,videos,images,releases`,
+    function (movie) {}
+);
