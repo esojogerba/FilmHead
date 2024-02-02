adf71a28410b776eb5c1aea4e6ab3750b18e1831
esojogerba <esojogerba@users.noreply.github.com>
2024-02-01 19:25:07 -0600

    Set details banner HTML.

--- assets/js/movie-details.js
+++ assets/js/movie-details.js
@@ -39,5 +39,97 @@ fetchDataFromAPI(
         // Create details-banner <section>
         const detailsBanner = document.createElement("section");
         detailsBanner.classList.add("banner", "details-banner");
+
+        // Sets details-banner <section> HTML.
+        // Uses template literals to inject movie data retrieved from API into the HTML.
+        // TODO : Add link to Add To Folder button.
+        detailsBanner.innerHTML = `
+            <div class="backdrop-image" style="background-image: url('${imageBaseURL}
+            ${"w1280" || "original"}${backdrop_path || poster_path}');"></div>
+
+            <div class="banner-columns">
+                <div class="banner-left-column">
+                    <figure class="poster-box details-poster">
+                        <img
+                            src="${imageBaseURL}w342${poster_path}"
+                            alt="${title}"
+                            class="img-cover"
+                        />
+                    </figure>
+
+                    <a
+                        class="btn btn-text-icon"
+                        href=""
+                        onclick=""
+                    >
+                        <span>Add to Folder</span>
+
+                        <svg class="material-icon" id="details-add-svg">
+                            <use
+                                xlink:href="/assets/images/icons.svg#add-icon"
+                            />
+                        </svg>
+                    </a>
+                </div>
+
+                <div class="banner-right-column">
+                    <h2 class="banner-heading details-heading">
+                        ${title}
+                    </h2>
+
+                    <div class="details-meta">
+                        <div class="meta-list">
+                            <div class="meta-item">${
+                                release_date.split("-")[0]
+                            }</div>
+
+                            <div class="meta-item">${runtime}mins</div>
+
+                            <div class="meta-item card-badge">${certification}</div>
+
+                            <div class="meta-item">
+                                <img
+                                    src="/assets/images/star-icon.svg"
+                                    width="20"
+                                    height="20"
+                                    alt="Rating"
+                                />
+
+                                <span>${vote_average.toFixed(1)}</span>
+                            </div>
+                        </div>
+
+                        <p class="detials-genre">
+                            Science Fiction · Drama · Thriller
+                        </p>
+                    </div>
+
+                    <p class="details-text">
+                        In the smog-choked dystopian Los Angeles of
+                        2019, blade runner Rick Deckard is called out of
+                        retirement to terminate a quartet of replicants
+                        who have escaped to Earth seeking their creator
+                        for a way to extend their short life spans.
+                    </p>
+
+                    <div class="details-cast">
+                        <p class="cast-title">Starring</p>
+
+                        <p class="cast-body">
+                            Harrison Ford, Rutger Hauer, Sean Young,
+                            Edward James Olmos, M. Emmet Walsh, Daryl
+                            Hannah, William Sanderson, Brion James, Joe
+                            Turkel, Joanna Cassidy
+                        </p>
+                    </div>
+
+                    <div class="details-director">
+                        <p class="director-title">Directed By</p>
+
+                        <p class="director-body">Ridley Scott</p>
+                    </div>
+                </div>
+            </div>
+        `;
     }
 );
