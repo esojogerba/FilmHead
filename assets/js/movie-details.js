6a7b788624d21348de1609a3533948a1f9d2fb3e
esojogerba <esojogerba@users.noreply.github.com>
2024-02-06 21:01:16 -0600

    Reorganized code.

--- assets/js/movie-details.js
+++ assets/js/movie-details.js
@@ -104,6 +104,173 @@ const filterVideos = function (videoList) {
     );
 };
 
+// Retrieves movie details using the provided movieId.
+fetchDataFromAPI(
+    `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}&append_to_response=casts,videos,images,releases`,
+    function (movie) {
+        // Stores data for current movie into movie.
+        const {
+            backdrop_path,
+            poster_path,
+            title,
+            release_date,
+            runtime,
+            vote_average,
+            releases: {
+                countries: [{ certification }],
+            },
+            genres,
+            overview,
+            casts: { cast, crew },
+            videos: { results: videos },
+        } = movie;
+
+        // Sets document title to movie title.
+        document.title = `${title} - FilmHead`;
+
+        // Create details-banner <section>
+        const detailsBanner = document.createElement("section");
+        detailsBanner.classList.add("banner", "details-banner");
+
+        // Sets details-banner <section> HTML.
+        // Uses template literals to inject movie data retrieved from API into the HTML.
+        // TODO : Add link to Add To Folder button.
+        detailsBanner.innerHTML = `
+            <div class="backdrop-image" style="background-image: linear-gradient(var(--details-overlay)), url('${imageBaseURL}w1280${backdrop_path}');"></div>
+
+            <div class="banner-columns">
+                <div class="banner-left-column">
+                    <figure class="poster-box details-poster">
+                        <img
+                            src="${imageBaseURL}w342${poster_path}"
+                            alt="${title}"
+                            class="img-cover"
+                        />
+                    </figure>
+
+                    <a
+                        class="btn btn-text-icon"
+                        href=""
+                        onclick=""
+                    >
+                        <span>Add to Folder</span>
+
+                        <svg class="material-icon" id="details-add-svg">
+                            <use
+                                xlink:href="/assets/images/icons.svg#add-icon"
+                            />
+                        </svg>
+                    </a>
+                </div>
+
+                <div class="banner-right-column">
+                    <h2 class="banner-heading details-heading">
+                        ${title}
+                    </h2>
+
+                    <div class="details-meta">
+                        <div class="meta-list">
+                            <div class="meta-item">${
+                                release_date.split("-")[0]
+                            }</div>
+
+                            <div class="meta-item">
+                                ${calcRuntimeHours(runtime)}
+                                ${calcRuntimeMinutes(runtime)}
+                            </div>
+
+                            <div class="meta-item card-badge">${certification}</div>
+
+                            <div class="meta-item">
+                                <img
+                                    src="/assets/images/star-icon.svg"
+                                    width="20"
+                                    height="20"
+                                    alt="Rating"
+                                />
+
+                                <span>${vote_average.toFixed(1)}</span>
+                            </div>
+                        </div>
+
+                        <p class="details-genre">
+                            ${getGenres(genres)}
+                        </p>
+                    </div>
+
+                    <p class="details-text">
+                        ${overview}
+                    </p>
+
+                    <div class="details-cast">
+                        <p class="cast-title">Starring</p>
+
+                        <p class="cast-body">
+                            ${getCasts(cast)}
+                        </p>
+                    </div>
+
+                    <div class="details-director">
+                        <p class="director-title">Directed By</p>
+
+                        <p class="director-body">
+                            ${getDirectors(crew)}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        `;
+
+        // Pushes the completed details section into the page.
+        pageContent.appendChild(detailsBanner);
+
+        // Create trailers & clips <section>
+        const clips = document.createElement("section");
+        clips.classList.add("media-scroll", "container");
+
+        clips.innerHTML = `
+            <div class="media-scroll-title-wrapper">
+                <h3 class="media-scroll-title">Trailers & Clips</h3>
+            </div>
+
+            <div class="media-slider-list">
+                <div class="slider-list-inner">
+                </div>
+            </div>
+         `;
+
+        const clipsSliderInner = clips.querySelector(".slider-list-inner");
+
+        // Creates videos section of the details page.
+        for (const { key, name } of filterVideos(videos)) {
+            // Creates video-card <div>.
+            const videoCard = document.createElement("div");
+            videoCard.classList.add("video-card");
+
+            // Sets video-card <div> HTML.
+            // Uses template literals to inject video data retrieved from API into the HTML.
+            videoCard.innerHTML = `
+                <iframe width="500"  height="294" 
+                src="https://www.youtube.com/embed/${key}?theme=dark&color=white&rel=0" 
+                frameborder="0" allowfullscreen="1" title="${name}" class="img-cover" 
+                loading="lazy"></iframe>
+            `;
+
+            // Adds the completed video card into slider-inner.
+            clipsSliderInner.appendChild(videoCard);
+        }
+
+        // Pushes the completed clips & trailers section into the page.
+        pageContent.appendChild(clips);
+
+        // Adds Available On <section>.
+        fetchDataFromAPI(
+            `https://api.themoviedb.org/3/movie/${movieId}/watch/providers?api_key=${API_KEY}&region=US`,
+            watchPlatforms
+        );
+    }
+);
+
 const watchPlatforms = function (watchData) {
     // Creates the Available On <section>.
     const availableOn = document.createElement("section");
@@ -289,170 +456,3 @@ const addSuggestedMovies = function ({ results: movieList }) {
         pageContent.appendChild(suggestedMovies);
     }
 };
-
-// Retrieves movie details using the provided movieId.
-fetchDataFromAPI(
-    `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}&append_to_response=casts,videos,images,releases`,
-    function (movie) {
-        // Stores data for current movie into movie.
-        const {
-            backdrop_path,
-            poster_path,
-            title,
-            release_date,
-            runtime,
-            vote_average,
-            releases: {
-                countries: [{ certification }],
-            },
-            genres,
-            overview,
-            casts: { cast, crew },
-            videos: { results: videos },
-        } = movie;
-
-        // Sets document title to movie title.
-        document.title = `${title} - FilmHead`;
-
-        // Create details-banner <section>
-        const detailsBanner = document.createElement("section");
-        detailsBanner.classList.add("banner", "details-banner");
-
-        // Sets details-banner <section> HTML.
-        // Uses template literals to inject movie data retrieved from API into the HTML.
-        // TODO : Add link to Add To Folder button.
-        detailsBanner.innerHTML = `
-            <div class="backdrop-image" style="background-image: linear-gradient(var(--details-overlay)), url('${imageBaseURL}w1280${backdrop_path}');"></div>
-
-            <div class="banner-columns">
-                <div class="banner-left-column">
-                    <figure class="poster-box details-poster">
-                        <img
-                            src="${imageBaseURL}w342${poster_path}"
-                            alt="${title}"
-                            class="img-cover"
-                        />
-                    </figure>
-
-                    <a
-                        class="btn btn-text-icon"
-                        href=""
-                        onclick=""
-                    >
-                        <span>Add to Folder</span>
-
-                        <svg class="material-icon" id="details-add-svg">
-                            <use
-                                xlink:href="/assets/images/icons.svg#add-icon"
-                            />
-                        </svg>
-                    </a>
-                </div>
-
-                <div class="banner-right-column">
-                    <h2 class="banner-heading details-heading">
-                        ${title}
-                    </h2>
-
-                    <div class="details-meta">
-                        <div class="meta-list">
-                            <div class="meta-item">${
-                                release_date.split("-")[0]
-                            }</div>
-
-                            <div class="meta-item">
-                                ${calcRuntimeHours(runtime)}
-                                ${calcRuntimeMinutes(runtime)}
-                            </div>
-
-                            <div class="meta-item card-badge">${certification}</div>
-
-                            <div class="meta-item">
-                                <img
-                                    src="/assets/images/star-icon.svg"
-                                    width="20"
-                                    height="20"
-                                    alt="Rating"
-                                />
-
-                                <span>${vote_average.toFixed(1)}</span>
-                            </div>
-                        </div>
-
-                        <p class="details-genre">
-                            ${getGenres(genres)}
-                        </p>
-                    </div>
-
-                    <p class="details-text">
-                        ${overview}
-                    </p>
-
-                    <div class="details-cast">
-                        <p class="cast-title">Starring</p>
-
-                        <p class="cast-body">
-                            ${getCasts(cast)}
-                        </p>
-                    </div>
-
-                    <div class="details-director">
-                        <p class="director-title">Directed By</p>
-
-                        <p class="director-body">
-                            ${getDirectors(crew)}
-                        </p>
-                    </div>
-                </div>
-            </div>
-        `;
-
-        // Pushes the completed details section into the page.
-        pageContent.appendChild(detailsBanner);
-
-        // Create trailers & clips <section>
-        const clips = document.createElement("section");
-        clips.classList.add("media-scroll", "container");
-
-        clips.innerHTML = `
-            <div class="media-scroll-title-wrapper">
-                <h3 class="media-scroll-title">Trailers & Clips</h3>
-            </div>
-
-            <div class="media-slider-list">
-                <div class="slider-list-inner">
-                </div>
-            </div>
-         `;
-
-        const clipsSliderInner = clips.querySelector(".slider-list-inner");
-
-        // Creates videos section of the details page.
-        for (const { key, name } of filterVideos(videos)) {
-            // Creates video-card <div>.
-            const videoCard = document.createElement("div");
-            videoCard.classList.add("video-card");
-
-            // Sets video-card <div> HTML.
-            // Uses template literals to inject video data retrieved from API into the HTML.
-            videoCard.innerHTML = `
-                <iframe width="500"  height="294" 
-                src="https://www.youtube.com/embed/${key}?theme=dark&color=white&rel=0" 
-                frameborder="0" allowfullscreen="1" title="${name}" class="img-cover" 
-                loading="lazy"></iframe>
-            `;
-
-            // Adds the completed video card into slider-inner.
-            clipsSliderInner.appendChild(videoCard);
-        }
-
-        // Pushes the completed clips & trailers section into the page.
-        pageContent.appendChild(clips);
-
-        // Adds Available On <section>.
-        fetchDataFromAPI(
-            `https://api.themoviedb.org/3/movie/${movieId}/watch/providers?api_key=${API_KEY}&region=US`,
-            watchPlatforms
-        );
-    }
-);
