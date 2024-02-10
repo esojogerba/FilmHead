4a759bac69d14b9d8349a523968396ed415919e7
esojogerba <esojogerba@users.noreply.github.com>
2024-02-10 17:27:27 -0600

    Variables for media-grip page.

--- assets/js/media-grid.js
+++ assets/js/media-grid.js
@@ -8,6 +8,15 @@ import { createMediaCard } from "./media-card.js";
 const genreName = window.localStorage.getItem("genreName");
 // Retrieve url parameter from local storage.
 const urlParam = window.localStorage.getItem("urlParam");
+// Retrieve media type from local storage.
+const mediaType = window.localStorage.getItem("mediaType");
 
 // Retrieves the page's content.
 const pageContent = document.querySelector("[page-content]");
+
+// Variables for movie list page.
+let currentPage = 1;
+let totalPages = 0;
+
+// URL
+let fetchURL = `https://api.themoviedb.org/3/discover/${mediaType}?api_key=${API_KEY}&sort_by=popularity.desc&include_adult=false&page=${currentPage}&${urlParam}`;
