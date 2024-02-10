e03f4ed9dbc77154615e435a69352e76122d10ea
esojogerba <esojogerba@users.noreply.github.com>
2024-02-10 17:46:03 -0600

    Media grid fetch function initialized.

--- assets/js/media-grid.js
+++ assets/js/media-grid.js
@@ -20,3 +20,16 @@ let totalPages = 0;
 
 // URL
 let fetchURL = `https://api.themoviedb.org/3/discover/${mediaType}?api_key=${API_KEY}&sort_by=popularity.desc&include_adult=false&page=${currentPage}&${urlParam}`;
+
+// Creates a media grid for the genre selected from the dropdown.
+fetchDataFromAPI(fetchURL, function ({ results: mediaList, total_pages }) {
+    // Set total pages.
+    totalPages = total_pages;
+
+    // Set title of document to the type of media list being loaded.
+    if (mediaType == "movie") {
+        document.title = `${genreName} Movies - FilmHead`;
+    } else {
+        document.title = `${genreName} Shows - FilmHead`;
+    }
+});
