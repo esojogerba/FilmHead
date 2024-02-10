f93d211c0a63084a7c8ada698caf1063cdedd080
esojogerba <esojogerba@users.noreply.github.com>
2024-02-10 17:23:34 -0600

    Added media type parameter to getMediaGrid.

--- assets/js/movies.js
+++ assets/js/movies.js
@@ -283,7 +283,7 @@ const buildPageHeader = function () {
             // OnClick attribute used to create a media list when a genre link is clicked.
             link.setAttribute(
                 "onClick",
-                `getMovieGrid("with_genres=${genreId}", "${genreName}")`
+                `getMovieGrid("with_genres=${genreId}", "${genreName}", "movie")`
             );
 
             // Gives resulting media list page the title of the genre selected.
