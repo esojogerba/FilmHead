7bf0f05cabd624983179d07f7bf580be5b6e1436
esojogerba <esojogerba@users.noreply.github.com>
2024-02-10 16:32:01 -0600

    Retrieve data from local storage.

--- assets/js/media-grid.js
+++ assets/js/media-grid.js
@@ -3,3 +3,8 @@
 // Imports
 import { API_KEY, imageBaseURL, fetchDataFromAPI } from "./api.js";
 import { createMediaCard } from "./media-card.js";
+
+// Retrieve genre name of movie list from local storage.
+const genreName = window.localStorage.getItem("genreName");
+// Retrieve url parameter from local storage.
+const urlParam = window.localStorage.getItem("urlParam");
