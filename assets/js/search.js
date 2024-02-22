c4411d35b3fa1222bc80a7620f59a51646cdab4c
esojogerba <esojogerba@users.noreply.github.com>
2024-02-22 17:34:28 -0600

    Add event listener to both search fields.

--- assets/js/search.js
+++ assets/js/search.js
@@ -17,4 +17,23 @@ export function search() {
     const searchModal = document.createElement("div");
     searchModal.classList.add("search-modal");
     document.querySelector("main").appendChild(searchModal);
+
+    // Timeout variable for loading data.
+    let searchTimeout;
+
+    // Start searching for media when there is an input in the search field.
+    // Refreshes search every time a new character is added to input value.
+    searchFields.forEach((element) => {
+        element.addEventListener("input", function () {
+            // If search field is empty, remove active class.
+            if (!element.value.trim()) {
+                searchModal.classList.remove("active");
+                clearTimeout(searchTimeout);
+                return;
+            }
+
+            // Reset timeout.
+            clearTimeout(searchTimeout);
+        });
+    });
 }
