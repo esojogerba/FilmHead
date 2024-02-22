051861fb8390fe3ba5a35e1fdffbce31872efbc8
esojogerba <esojogerba@users.noreply.github.com>
2024-02-22 14:23:05 -0600

    Created search modal.

--- assets/js/search.js
+++ assets/js/search.js
@@ -6,10 +6,15 @@ import { createMediaCard } from "./media-card.js";
 
 // Exports search functionality.
 export function search() {
-    // Retrieve the search field.
-    const searchField = Array.prototype.slice.call(
+    // Retrieve the search fields.
+    const searchFields = Array.prototype.slice.call(
         document.getElementsByClassName("search-field")
     );
 
-    console.log(searchField);
+    console.log(searchFields);
+
+    // Create the search-modal <div>
+    const searchModal = document.createElement("div");
+    searchModal.classList.add("search-modal");
+    document.querySelector("main").appendChild(searchModal);
 }
