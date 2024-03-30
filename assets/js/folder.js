3b259090131ab3f6a009a09a9ca0d0939dc8f2f4
esojogerba <esojogerba@users.noreply.github.com>
2024-03-30 04:14:50 -0500

    Create  a scroll item for each movie genre.

--- assets/js/folder.js
+++ assets/js/folder.js
@@ -3,27 +3,41 @@
 // Imports
 import { API_KEY, imageBaseURL, fetchDataFromAPI } from "./api.js";
 
-//  API call to get movie genres
-fetchDataFromAPI(
-    `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`,
-    movieGenres
-);
-
 const movieGenres = function ({ genres }) {
     // Retrieve movie scroll inside filter pop up from doc
     const movieScroll = document.querySelector(".movie-genres-scroll");
 
     // Holds all movie genres and their ID's.
-    const movieGenres = {};
+    const movieGenresList = {};
 
     // Iterates through each movie genre in the JSON file.
     for (const { id, name } of genres) {
         // Make a key value pair
-        movieGenres[id] = name;
+        movieGenresList[id] = name;
+    }
+
+    // Iterates through each entry in genreList.
+    for (const [genreId, genreName] of Object.entries(movieGenresList)) {
+        // Create scroll item
+        const scrollItem = document.createElement("div");
+        scrollItem.classList.add("filter-by-scroll-item");
+        // Set scroll item inner HTML.
+        scrollItem.innerHTML = `
+            <a class="add-to-folder-scroll-btn" href=""></a>
+            <span>${genreName}</span>
+        `;
+        // Add new scroll item to movieScroll
+        movieScroll.appendChild(scrollItem);
     }
 
     // API call to get show genres
 };
+
+//  API call to get movie genres
+fetchDataFromAPI(
+    `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`,
+    movieGenres
+);
 
 // Edit inner HTML of movie genre dropdown
 
