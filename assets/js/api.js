22451140b7f7516930e622b641220a3c3f2c8c10
esojogerba <esojogerba@users.noreply.github.com>
2024-08-28 01:01:03 -0500

    Added additional param to fetch data function.

--- assets/js/api.js
+++ assets/js/api.js
@@ -10,13 +10,18 @@ const imageBaseURL = "http://image.tmdb.org/t/p/";
 // Fetches data from API using a url.
 // Passes the results in JSON format into a callback function,
 // along with an optional parameter if applicable.
-const fetchDataFromAPI = function (url, callback, optionalParam) {
+const fetchDataFromAPI = function (
+    url,
+    callback,
+    optionalParam1,
+    optionalParam2
+) {
     // Fetch data from URL.
     fetch(url)
         // Format results into JSON format.
         .then((response) => response.json())
         // Callback function provided as parameter is called.
-        .then((data) => callback(data, optionalParam));
+        .then((data) => callback(data, optionalParam1, optionalParam2));
 };
 
 // Export functionalities and constant variables.
