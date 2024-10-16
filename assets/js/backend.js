8f0e68ab9048cc5711528083488fe4d8f9ce1254
esojogerba <esojogerba@users.noreply.github.com>
2024-10-15 22:03:59 -0500

    Exported function.

--- assets/js/backend.js
+++ assets/js/backend.js
@@ -4,7 +4,12 @@
 function initializeLocalStorage() {
     if (!localStorage.getItem("folders")) {
         localStorage.setItem("folders", JSON.stringify([]));
+        console.log("Folders created");
     }
+    console.log("Folders exists");
 }
 
-// ////////////////////
+// Export functions to window.Backend
+window.Backend = {
+    initializeLocalStorage,
+};
