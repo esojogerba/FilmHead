ac1e5db658cec0041e768f24c40965577daea23a
esojogerba <esojogerba@users.noreply.github.com>
2024-10-16 21:57:16 -0500

    createFolder function.

--- assets/js/backend.js
+++ assets/js/backend.js
@@ -14,8 +14,32 @@ function getFolders() {
     return JSON.parse(localStorage.getItem("folders"));
 }
 
+// Create a New Folder
+function createFolder(name) {
+    const inputValue = document.getElementsByClassName(
+        "create-folder-input"
+    ).value;
+
+    const folders = getFolders();
+    if (folders.some((folder) => folder.name === name)) {
+        throw new Error("Folder name already exists.");
+    }
+
+    const newFolder = {
+        name: name,
+        entries: 0,
+        percent_complete: "0%",
+        poster_urls: [],
+        media: [],
+    };
+
+    folders.push(newFolder);
+    localStorage.setItem("folders", JSON.stringify(folders));
+}
+
 // Export functions to window.Backend
 window.Backend = {
     initializeLocalStorage,
     getFolders,
+    createFolder,
 };
